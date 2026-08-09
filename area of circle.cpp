e518c0c04e6ff55f6b6b3fb0f4d9e576cb7e8aad
Daksh Pasricha <dakshpasricha64@gmail.com>
2026-08-10 00:30:28 +0530

    Add files via upload

---
+++ area of circle.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>		
+int main()
+{
+    int r, area;
+    printf("enter the radius =");
+    scanf("%d",&r);
+    area= 3.14*r*r;
+    printf("r=%d\narea=%d",r,area);
+    return 0;
+}
